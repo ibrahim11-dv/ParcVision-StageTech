297d683abeee90a99383ee9baa38c385cbacdd07
ibrahim <ibrahim.chehlafi.07@gmail.com>
2026-04-19 16:56:43 +0100

    initial commit

---
+++ src/main/java/com/ibrahim/parcvision/ParcVisionApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.parcvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibrahim/parcvision/ParcVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.parcvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
