0bc14b7fea3bb9287a6ba0d7dcd497b40badb98e
ibrahim <ibrahim.chehlafi.07@gmail.com>
2026-04-29 12:45:39 +0100

    initial config, still working on it

--- src/main/java/com/ibrahim/parcvision/configuration/SecurityConfig.java
+++ src/main/java/com/ibrahim/parcvision/configuration/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.ibrahim.parcvision.configuration;
 
+import com.ibrahim.parcvision.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http){
@@ -33,4 +46,15 @@ public class SecurityConfig {
         //.httpBasic(Customizer.withDefaults());
         return http.build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/ibrahim/parcvision/service/CustomUserDetailsService.java
+++ src/main/java/com/ibrahim/parcvision/service/CustomUserDetailsService.java
@@ -1,16 +1,25 @@
 package com.ibrahim.parcvision.service;
 
+import com.ibrahim.parcvision.entity.Utilisateur;
 import com.ibrahim.parcvision.repository.UtilisateurRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private UtilisateurRepository utilisateurRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return utilisateurRepository.findByEmail(username).orElseThrow(()->
+        Utilisateur user = utilisateurRepository.findByEmail(username).orElseThrow(()->
                 new UsernameNotFoundException("Email or Code does not exist"));
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
     }
 }
